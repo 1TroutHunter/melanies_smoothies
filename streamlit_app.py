04e391acffdcbddb44ca06e1495fe50ce6bffb24
1TroutHunter <aslyons@alaska.edu>
2025-07-18 15:15:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-#from snowflake.snowpark.context import get_active_session
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
@@ -40,3 +41,4 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
+
